3e51a5d4b61253db7abc35374bbdd5b2d25cf5cc
ywnkm <93203373+ywnkm@users.noreply.github.com>
2022-01-21 17:07:17 +0800

    Initial buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+
+    const val kotlinCompiler = "1.6.10"
+    const val kotlinxSerialization = "1.3.1"
+    const val kotlinxCoroutines = "1.5.2"
+    const val kotlinxAtomicfu = "0.17.0"
+    const val kotlinxIO = "0.1.1"
+    const val ktor = "1.6.5"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.aliyun.com/nexus/content/groups/public/")
+        mavenCentral()
+        google()
+    }
+
+    plugins {
+        val versionsText = rootDir.resolve("buildSrc/src/main/kotlin/Versions.kt").readText()
+        fun version(name: String): String {
+
+            return versionsText.lineSequence()
+                .map { it.trim() }
+                .single { it.startsWith("const val $name") }
+                .substringAfter('"', "")
+                .substringBefore('"', "")
+                .also {
+                    check(it.isNotBlank())
+                    logger.debug("$name=$it")
+                }
+        }
+
+        kotlin("jvm") version version("kotlinCompiler")
+        kotlin("plugin.serialization") version version("kotlinCompiler")
+    }
+}
