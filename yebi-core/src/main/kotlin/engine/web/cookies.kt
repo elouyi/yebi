ef81d279e2cb60e85b717dae5c42b9547947605e
ywnkm <yw@elouyi.com>
2022-03-22 22:26:51 +0800

    engine

--- yebi-core/src/main/kotlin/engine/web/cookies.kt
+++ yebi-core/src/main/kotlin/engine/web/cookies.kt
@@ -64,9 +64,3 @@ public fun HttpRequestBuilder.withUserCookie(cookie: UserCookie) {
     cookie("DedeUserID__ckMd5",cookie.dedeUserIDCkMd5)
     cookie("bili_jct",cookie.bili_jct)
 }
-
-public fun main() {
-    val c = "_uuid=C8E14B49-E497-C33B-A5C6-1091461034CAFE98300infoc; buvid3=3CBDF1D8-D8EC-4A97-8CF3-2C7F4BC4177C167639infoc; b_nut=1637760898; video_page_version=v_old_home; rpdid=|(JYYRlYR~Yu0J'uYJ~~Y|mkm; i-wanna-go-back=-1; CURRENT_BLACKGAP=0; LIVE_BUVID=AUTO3216384519439372; blackside_state=0; fingerprint3=c9640a46a3fe6a3c8b0b8d92fd96d929; fingerprint_s=d4243ad47f2f5cd83e92fd91a323c3d9; buvid_fp_plain=undefined; sid=lkhsipbk; DedeUserID=84301837; DedeUserID__ckMd5=70c09f12a2807587; bili_jct=fbf688b67fa881df35b0daee34cf7ba7; b_ut=5; buvid4=41E1D160-6183-AEB8-4726-4347B85206C498376-022012418-Z+jkrkK4bspKpHAffJmSdQ%3D%3D; CURRENT_QUALITY=112; nostalgia_conf=-1; fingerprint=05da3daf5605966fb5c8f1db5a8e799a; buvid_fp=05da3daf5605966fb5c8f1db5a8e799a; PVID=1; bp_video_offset_84301837=640054344564080600; CURRENT_FNVAL=80; innersign=0; b_lsid=6B7DAB72_17FB1EB28FD"
-    val cookie = c.getUserCookie()
-    println(cookie)
-}
