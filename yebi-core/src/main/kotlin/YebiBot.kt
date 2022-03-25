b9d0b9afdb1ce7a90e593fa590a2bccd19e7146b
ywnkm <yw@elouyi.com>
2022-03-25 10:30:36 +0800

    add doc

--- yebi-core/src/main/kotlin/YebiBot.kt
+++ yebi-core/src/main/kotlin/YebiBot.kt
@@ -5,6 +5,24 @@ import com.elouyi.yebi.engine.YebiEngineConfig
 import com.elouyi.yebi.engine.YebiEngineFactory
 import com.elouyi.yebi.feature.Attributes
 
+/**
+ * 构造一个 [YebiBot] 实例
+ *
+ * example:
+ * ```kotlin
+ * val yebi = Yebi(YWE) {  // 需要 yebi-engine-web 依赖
+ *     // 通用配置
+ *     install(Live) {  // 需要 yebi-live 依赖
+ *
+ *     }
+ *     engine {
+ *         // 具体 engine 的配置
+ *     }
+ * }
+ * ```
+ * @param engineFactory 引擎工厂，通常是一个 Object
+ * @param block 配置
+ */
 public fun <T : YebiEngineConfig> YebiBot(
     engineFactory: YebiEngineFactory<T>,
     block: YebiConfig<T>.() -> Unit = {}
