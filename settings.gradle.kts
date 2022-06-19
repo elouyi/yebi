6113433e57a3acb78805be1d37d341c9d9d8159b
ywnkm <yw@elouyi.com>
2022-06-19 13:02:11 +0800

    init

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "yebi"
+
+include(":yebi-core")
---
+++ yebi-core/build.gradle.kts
@@ -0,0 +1,13 @@
+
+plugins {
+    kotlin("multiplatform")
+}
+
+apply(from = rootProject.file("gradle/yebi-common.gradle"))
+apply(from = rootProject.file("gradle/compile-multiplatform.gradle"))
+
+kotlin {
+
+
+
+}
